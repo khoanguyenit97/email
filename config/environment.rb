f230d6ffe3afa9cfd8c9f159c27fa6b104ea7b6a
khoanda10 <khoanda10@gmail.com>
2022-03-23 16:52:20 +0700

    fasf

--- config/environment.rb
+++ config/environment.rb
@@ -3,28 +3,14 @@ require_relative "application"
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
 ActionMailer::Base.smtp_settings = {
-  domain: 'heroku.com',
-  address:        'smtp.sendgrid.net',
-  port:            587,
-  authentication: :plain,
-  user_name:      'apikey',
-  password:       ENV['SENDGRID_API_KEY']
+  :address => "smtp.sendgrid.net",
+  :port => 587,
+  :domain => "heroku.com",
+  :ssl => true,
+  :enable_starttls_auto => true,
+  :authentication => :login,
+  :user_name => ENV['SENDGRID_USERNAME'],
+  :password => ENV['SENDGRID_PASSWORD']
 }
-# ActionMailer::Base.smtp_settings = {
-#
-#   :address => 'smtp.sendgrid.net',
-#
-#   :port => '587',
-#
-#   :authentication => :plain,
-#
-#   :user_name => ENV['SENDGRID_USERNAME'],
-#
-#   :password => ENV['SENDGRID_PASSWORD'],
-#
-#   :domain => 'heroku.com',
-#
-#   :enable_starttls_auto => true
-#
-# }
