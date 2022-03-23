75042c77049c65b29e66e12ca87c5d0666714589
khoanda10 <khoanda10@gmail.com>
2022-03-23 20:59:51 +0700

    kasfj

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -11,17 +11,8 @@ Rails.application.configure do
   # and those relying on copy on write to perform better.
   # Rake tasks automatically ignore this option for performance.
   config.eager_load = true
-  ActionMailer::Base.smtp_settings = {
-    :address => 'smtp.sendgrid.net',
-    :port => '587',
-    :authentication => :plain,
-    :user_name => Rails.application.credentials.dig(:user_name),
-    :password => Rails.application.credentials.dig(:password),
-    :domain => 'heroku.com',
-    :enable_starttls_auto => true
-  }
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.default_url_options ={ :host => 'khoa-it97-photo-app.herokuapp.com', :protocol => 'https' }
+  config.action_mailer.default_url_options = { :host => '<https://khoa-it97-photo-app.herokuapp.com>', :protocol => 'http' }
   # Full error reports are disabled and caching is turned on.
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
