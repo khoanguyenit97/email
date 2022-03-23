baa0ad43b8ed335df90fb4f862ea64f5396bd7da
khoanda10 <khoanda10@gmail.com>
2022-03-23 17:25:02 +0700

    fsdf

--- config/environment.rb
+++ config/environment.rb
@@ -8,7 +8,6 @@ ActionMailer::Base.smtp_settings = {
   :address => "smtp.sendgrid.net",
   :port => 587,
   :domain => "heroku.com",
-  :ssl => true,
   :enable_starttls_auto => true,
   :authentication => :login,
   :user_name => ENV['SENDGRID_USERNAME'],
