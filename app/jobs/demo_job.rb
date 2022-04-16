09a8739966769232631aae1a5b09e1b02f843935
khoanda10 <khoanda10@gmail.com>
2022-04-16 11:55:27 +0700

    Add active job(application job rails)

---
+++ app/jobs/demo_job.rb
@@ -0,0 +1,10 @@
+class DemoJob < ApplicationJob
+  def perform
+    puts 'This is demo job.'
+  end
+end
+
+# asynchronous -> bat dong bo, viet tat la async
+# chay tuan tu, word chay done, moi toi excel
+# synchronous -> dong bo, viet tat la sync
+# chay dong thoi
